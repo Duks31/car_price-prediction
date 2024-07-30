dd3b6e9a362501984d571a92afc46a68a35c20a7
Duks31 <chidubemjan31@gmail.com>
2024-07-30 12:57:27 +0100

    added gradio ui

--- car_pred/main.py
+++ car_pred/main.py
@@ -18,3 +18,21 @@ async def predict_price(car: CarPrices):
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port = 8000)
+
+# Sample Request
+# {
+#     "make": "Mercedes-Benz",
+#     "model": "M Class",
+#     "year_of_manufacture": 2018,
+#     "color": "White",
+#     "condition": "Nigerian Used",
+#     "mileage": 69224.0,
+#     "engine_size": 800009.0,    
+#     "registered_city": "LAGOS",
+#     "selling_condition": "Registered",
+#     "bought_condition": "Imported",
+#     "city": "Lekki",
+#     "body_type": "SUV",
+#     "fuel_type": "Petrol",
+#     "transmission": "Automatic"
+# }
